3ad053d65d1684c2d2bb3b0a752daf1d98d6cda7
Rails Udemy Course Juan Guerrero <guerrerojuan1991@yahoo.com>
2017-07-10 21:15:29 +0000

    Added search function to the foods database

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,11 @@
 class Food < ActiveRecord::Base
+    
+    def self.search(title)
+        if title
+            where('title LIKE ?', "%#{title}%")
+        else
+            all
+        end
+    end
+    
 end
